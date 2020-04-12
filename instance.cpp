17c6b4a82fa671cc49c8f063713c6d03c904bfbe
Matheus Abreu <matheusa596@gmail.com>
2020-04-12 07:14:54 -0300

    Exclusão de linhas e colunas.

--- instance.cpp
+++ instance.cpp
@@ -17,14 +17,28 @@ vector<int> unionSet(vector<int> s1, vector<int> s2) {
 	return uniao;
 }
 
-int diffSet(vector<int> s1, vector<int> s2) {
+vector<int> diffSet(vector<int> s1, vector<int> s2) {
 	vector<int> diff (s1.size());
 	vector<int>::iterator it;
 
 	it = set_difference(s1.begin(), s1.end(), s2.begin(), s2.end(), diff.begin());
 	diff.resize(it - diff.begin());
 
-	return diff.size();
+	return diff;
+}
+
+template <typename tipo> vector<tipo> deleteByIndexes(vector<tipo> vetor, vector<int> indices) {
+	vector<tipo> vetorNovo;
+	int i = 0, j = 0;
+	while (i < vetor.size()) {
+		if (j < indices.size() < indices[j] == i) {
+			j++;
+		} else {
+			vetorNovo.push_back(vetor[i]);
+		}
+		i++;
+	}
+	return vetorNovo;
 }
 
 class Instance {
@@ -32,10 +46,11 @@ class Instance {
 	public:
 		Instance(string caminho);
 		float greedy();
-		vector<vector<int>> construirCoberturas() const;
+		vector<vector<int>> getCoberturas() const;
 		int melhorCobertura(vector<vector<int>> coberturas, vector<int> universo) const;
 
-		void excluirColuna(int i);
+		vector<int> excluirColuna(int i);
+		void excluirLinhas(vector<int> linhas);
 
 		int n;
 		int m;
@@ -43,20 +58,51 @@ class Instance {
 		vector<vector<char>> matriz;
 		vector<vector<int>> coberturas;
 		vector<int> colunasExcluidas;
+		vector<int> linhasExcluidas;
+	private:
+		void construirCoberturas();
 	
 };
 
-void Instance::excluirColuna(int i) {
+void Instance::construirCoberturas() {
+	this->coberturas.clear();
+	for (int i = 0; i < this->n; i++) {
+		vector<int> s_i;
+		for (int j = 0; j < this->m; j++) {
+			if (this->matriz[j][i] != 0) {
+				s_i.push_back(j);
+			}
+		}
+		this->coberturas.push_back(s_i);
+	}
+}
+
+void Instance::excluirLinhas(vector<int> linhas) {
+	this->matriz = deleteByIndexes<vector<char>>(this->matriz, linhas);
+	this->linhasExcluidas.insert(this->linhasExcluidas.end(), linhas.begin(), linhas.end());
+	this->m -= linhas.size();
+	this->construirCoberturas();
+}
+
+vector<int> Instance::excluirColuna(int i) {
+
+	vector<int> linhasCobertas;
+
 	this->custos[i] = this->custos.back();
 	this->custos.pop_back();
 	this->coberturas[i] = this->coberturas.back();
 	this->coberturas.pop_back();
 	for (int k = 0; k < this->m; k++) {
+		if (this->matriz[k][i] == 1) {
+			linhasCobertas.push_back(k);
+		}
 		this->matriz[k][i] = this->matriz[k].back();
 		this->matriz[k].pop_back();
 	}
 	this->colunasExcluidas.push_back(i);
 	this->n--;
+
+	return linhasCobertas;
 }
 
 Instance::Instance(string caminho) {
@@ -85,18 +131,10 @@ Instance::Instance(string caminho) {
 		}
 	}
 
-	for (int i = 0; i < this->n; i++) {
-		vector<int> s_i;
-		for (int j = 0; j < this->m; j++) {
-			if (this->matriz[j][i] != 0) {
-				s_i.push_back(j);
-			}
-		}
-		this->coberturas.push_back(s_i);
-	}
+	this->construirCoberturas();
 }
 
-vector<vector<int>> Instance::construirCoberturas() const {
+vector<vector<int>> Instance::getCoberturas() const {
 	return this->coberturas;
 }
 
@@ -105,7 +143,7 @@ int Instance::melhorCobertura(vector<vector<int>> coberturas, vector<int> univer
 	int indMin = -1, tam_aux;
 
 	for (int i = 0; i < this->n; i++) {
-		tam_aux = diffSet(coberturas[i], universo);
+		tam_aux = diffSet(coberturas[i], universo).size();
 		if (tam_aux > 0) {
 			custo_aux =  this->custos[i] / tam_aux;
 			if (custo_aux < min_custo) {
@@ -120,7 +158,7 @@ int Instance::melhorCobertura(vector<vector<int>> coberturas, vector<int> univer
 
 /* https://www.geeksforgeeks.org/set-cover-problem-set-1-greedy-approximate-algorithm/ */
 float Instance::greedy() {
-	vector<vector<int>> coberturas = this->construirCoberturas();
+	vector<vector<int>> coberturas = this->getCoberturas();
 	vector<int> universo;
 	float custo = 0;
 
@@ -139,28 +177,28 @@ float Instance::greedy() {
 class LagrangeanSetCovering {
 	
 	public:
-		LagrangeanSetCovering(Instance &instancia, vector<float> &multiplicadores);
+		LagrangeanSetCovering(Instance &instancia);
 		float calcularLowerBound();
 		float heuristica();
+		void reduzir(float Z_UB, float Z_LB);
 
-
-		vector<float> &multiplicadores;
+		vector<float> multiplicadores;
 		Instance &instancia;
 		vector<float> C;
-		void excluirColunas(float Z_UB, float Z_LB);
+		float custoAcumulado;
 	
 };
 
-LagrangeanSetCovering::LagrangeanSetCovering(Instance &instancia, vector<float> &multiplicadores) :
-	instancia(instancia),
-	multiplicadores(multiplicadores) {
-
+LagrangeanSetCovering::LagrangeanSetCovering(Instance &instancia) :
+	instancia(instancia) {
+	this->multiplicadores = vector<float>(instancia.m);
+	this->custoAcumulado = 0;
 }
 
 float LagrangeanSetCovering::calcularLowerBound() {
 	this->C = vector<float>(this->instancia.n, 0.0);
 
-	float Z_LB = 0;
+	float Z_LB = this->custoAcumulado;
 
 	for (int i = 0; i < this->instancia.n; i++) {
 		this->C[i] = this->instancia.custos[i];
@@ -180,9 +218,9 @@ float LagrangeanSetCovering::calcularLowerBound() {
 }
 
 float LagrangeanSetCovering::heuristica() {
-	vector<vector<int>> coberturas = this->instancia.construirCoberturas();
+	vector<vector<int>> coberturas = this->instancia.getCoberturas();
 	vector<int> universo;
-	float custo = 0;
+	float custo = this->custoAcumulado;
 
 	for (int i = 0; i < this->instancia.n; i++) {
 		if (this->C[i] <= 0) {
@@ -202,12 +240,17 @@ float LagrangeanSetCovering::heuristica() {
 	return custo;
 }
 
-void LagrangeanSetCovering::excluirColunas(float Z_UB, float Z_LB) {
+void LagrangeanSetCovering::reduzir(float Z_UB, float Z_LB) {
 	for (int i = 0; i < this->instancia.n; i++) {
-		if (this->C[i] > 0 && Z_LB + this->instancia.custos[i] > Z_UB) {
-			this->C[i] = this->C.back();
-			this->C.pop_back();
+		if (this->C[i] > 0 && Z_LB + this->C[i] > Z_UB) {
 			this->instancia.excluirColuna(i);
+			break;
+		} else if (this->C[i] <= 0 && Z_LB - this->C[i] > Z_UB) {
+			cout << Z_LB << ' ' << Z_UB << endl;
+			this->custoAcumulado += this->instancia.custos[i];
+			vector<int> linhasParaExcluir = this->instancia.excluirColuna(i);
+			this->multiplicadores = deleteByIndexes<float>(this->multiplicadores, linhasParaExcluir);
+			this->instancia.excluirLinhas(linhasParaExcluir);
 			break;
 		}
 	}
@@ -220,7 +263,6 @@ int main(int argc, char const *argv[]) {
 	Instance instancia(argv[1]);
 
 	// passo 1
-
 	float pi = 2; // 0 < pi <= 2
 	const float MENOR_PI = 0.005;
 	const float PER_Z_UB = 1.02;
@@ -228,13 +270,9 @@ int main(int argc, char const *argv[]) {
 	const int N = 50; // 30
 	int i = 0;
 
-	vector<float> multiplicadores(instancia.m, 0);
-	//multiplicadores[0] = 1.5, multiplicadores[1] = 1.6, multiplicadores[2] = 2.2;
-
-	LagrangeanSetCovering lag(instancia, multiplicadores);
+	LagrangeanSetCovering lag(instancia);
 
 	float Z_UB = instancia.greedy(); // 6
-
 	// fim passo 1
 
 	float Z_MAX = -std::numeric_limits<float>::infinity();
@@ -272,8 +310,8 @@ int main(int argc, char const *argv[]) {
 
 		// passo 5
 		for (int i = 0; i < instancia.m; i++) {
-			candidatoValor = multiplicadores[i] + T * G[i];
-			multiplicadores[i] = candidatoValor > 0 ? candidatoValor : 0; 
+			candidatoValor = lag.multiplicadores[i] + T * G[i];
+			lag.multiplicadores[i] = candidatoValor > 0 ? candidatoValor : 0; 
 		}
 		// fim passo 5
 
@@ -283,12 +321,11 @@ int main(int argc, char const *argv[]) {
 			float custoHeuristica = lag.heuristica();
 			if (custoHeuristica < Z_UB) {
 				Z_UB = custoHeuristica;
-				lag.excluirColunas(Z_UB, Z_LB);
+				lag.reduzir(Z_UB, Z_LB);
 			}
 		} else {
 			iteracoes_sem_melhora += 1;
 		}
-
 		if (iteracoes_sem_melhora > N) {
 			iteracoes_sem_melhora = 0;
 			pi /= 2;
@@ -296,6 +333,7 @@ int main(int argc, char const *argv[]) {
 		i++;
 	}
 	cout << instancia.colunasExcluidas.size() << endl;
+	cout << instancia.linhasExcluidas.size() << endl;
 	cout << Z_UB << endl;
 	cout << i << endl;
 	cout << Z_MAX << endl;
